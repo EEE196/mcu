c89524d04e13dbb5e3d2735a03f35503093a04bf
unknown <mahikgot@gmail.com>
2023-03-22 19:25:36 +0800

    comment out starting of timer

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -97,11 +97,11 @@ int main(void)
   MX_USART1_UART_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  if (HAL_TIM_Base_Start_IT(&htim16) != HAL_OK)
+  /* if (HAL_TIM_Base_Start_IT(&htim16) != HAL_OK)
     {
-      /* Starting Error */
       Error_Handler();
     }
+  */
   /* USER CODE END 2 */
 
   /* Infinite loop */
