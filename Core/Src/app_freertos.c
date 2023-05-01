c4143bc927b8edb1b91cfbf03bc67eddf96dc78a
unknown <mahikgot@gmail.com>
2023-05-01 12:46:23 +0800

    initialize sensor tasks and queue

--- Core/Src/app_freertos.c
+++ Core/Src/app_freertos.c
@@ -45,16 +45,25 @@
 
 /* Private variables ---------------------------------------------------------*/
 /* USER CODE BEGIN Variables */
+typedef struct IP_TASK_COMMANDS
+{
+    uint8_t from_Task; /*0 - gps_so; 1 - PM; 2 - CO */
+    void *pvData; /* Holds or points to any data associated with the event. */
 
+} xIPStackEvent_t;
 /* USER CODE END Variables */
-osThreadId defaultTaskHandle;
+osThreadId PMHandle;
+osThreadId COHandle;
+osThreadId GPS_SOHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
 
 /* USER CODE END FunctionPrototypes */
 
-void StartDefaultTask(void const * argument);
+void PM_Task(void const * argument);
+void CO_Task(void const * argument);
+void GPS_SO_Task(void const * argument);
 
 void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
@@ -98,12 +107,21 @@ void MX_FREERTOS_Init(void) {
 
   /* USER CODE BEGIN RTOS_QUEUES */
   /* add queues, ... */
+  QueueHandle_t xQueueCollate = xQueueCreate( 10, sizeof( xIPStackEvent_t ) );
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
-  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+  /* definition and creation of PM */
+  osThreadDef(PM, PM_Task, osPriorityNormal, 0, 128);
+  PMHandle = osThreadCreate(osThread(PM), NULL);
+
+  /* definition and creation of CO */
+  osThreadDef(CO, CO_Task, osPriorityNormal, 0, 128);
+  COHandle = osThreadCreate(osThread(CO), NULL);
+
+  /* definition and creation of GPS_SO */
+  osThreadDef(GPS_SO, GPS_SO_Task, osPriorityNormal, 0, 128);
+  GPS_SOHandle = osThreadCreate(osThread(GPS_SO), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
   /* add threads, ... */
@@ -111,22 +129,58 @@ void MX_FREERTOS_Init(void) {
 
 }
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_PM_Task */
 /**
-  * @brief  Function implementing the defaultTask thread.
+  * @brief  Function implementing the PM thread.
   * @param  argument: Not used
   * @retval None
   */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void const * argument)
+/* USER CODE END Header_PM_Task */
+void PM_Task(void const * argument)
 {
-  /* USER CODE BEGIN StartDefaultTask */
+  /* USER CODE BEGIN PM_Task */
   /* Infinite loop */
   for(;;)
   {
     osDelay(1);
   }
-  /* USER CODE END StartDefaultTask */
+  /* USER CODE END PM_Task */
+}
+
+/* USER CODE BEGIN Header_CO_Task */
+/**
+* @brief Function implementing the CO thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_CO_Task */
+void CO_Task(void const * argument)
+{
+  /* USER CODE BEGIN CO_Task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END CO_Task */
+}
+
+/* USER CODE BEGIN Header_GPS_SO_Task */
+/**
+* @brief Function implementing the GPS_SO thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_GPS_SO_Task */
+void GPS_SO_Task(void const * argument)
+{
+  /* USER CODE BEGIN GPS_SO_Task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END GPS_SO_Task */
 }
 
 /* Private application code --------------------------------------------------*/
